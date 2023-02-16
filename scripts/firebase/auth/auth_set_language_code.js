796aeffbdaa0ca3cb1158e7377f0043db166948c
PillageDev <pillageyt@gmail.com>
2023-02-15 22:48:03 -0500

    :sparkles: Finally got login working

--- scripts/firebase/auth/auth_set_language_code.js
+++
@@ -1,6 +0,0 @@
-import { getAuth } from "firebase/auth";
-
-const auth = getAuth();
-auth.languageCode = 'it';
-// To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
