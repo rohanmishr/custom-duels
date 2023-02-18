700f794001a02e6d7f02c4f1193d3f6884f0c66b
rohanmishra <104006850+rohanmishr@users.noreply.github.com>
2023-02-18 14:28:21 +0000

    commit

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -7,7 +7,27 @@ function toggleWidgetSelection(){
     }
 }
 
+var keys =  ["total", "daily", "rapid", "blitz", "bullet"];
+var shownWidgets = [];
 setInterval(function(){
+    for(var i = 0; i < 5; i++){
+        if(document.getElementById(keys[i] + "-wins").checked){
+            shownWidgets.push("stats-" + keys[i] + "-wins");
+        }
+        if(document.getElementById(keys[i] + "-losses").checked){
+            shownWidgets.push("stats-" + keys[i] + "-losses");
+        }
+        if(document.getElementById(keys[i] + "-ties").checked){
+            shownWidgets.push("stats-" + keys[i] + "-ties");
+        }
+        if(document.getElementById(keys[i] + "-elo").checked){
+            shownWidgets.push("stats-" + keys[i] + "-elo");
+        }
+        if(document.getElementById(keys[i] + "-ws").checked){
+            shownWidgets.push("stats-" + keys[i] + "-ws");
+        }
+    }
+
     for(var i = 0; i < shownWidgets.length; i++){
         var show = document.getElementById(shownWidgets[i]);
         show.style.display = "block";
